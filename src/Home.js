be3f4ed16e9d5d3087d31aae5c9855600c35d5c7
Sporego <contact@filbert.io>
2020-09-23 13:14:25 -0400

    Issues 1st pass

--- src/Home.js
+++ src/Home.js
@@ -70,13 +70,13 @@ function Home() {
                     </ol>
                     <div class="carousel-inner">
                         <div class="carousel-item active">
-                        <img class="d-block w-100" src={require('./assets/img/backgrounds/1.jpg')} alt="First slide"/>
+                        <img class="d-block w-100" src='../public/img/1.jpg' alt="First slide"/>
                         </div>
                         <div class="carousel-item">
-                        <img class="d-block w-100" src={require('./assets/img/backgrounds/2.jpg')} alt="Second slide"/>
+                        <img class="d-block w-100" src='./assets/img/backgrounds/2.jpg' alt="Second slide"/>
                         </div>
                         <div class="carousel-item">
-                        <img class="d-block w-100" src={require('./assets/img/backgrounds/3.jpg')} alt="Third slide"/>
+                        <img class="d-block w-100" src='./assets/img/backgrounds/3.jpg' alt="Third slide"/>
                         </div>
                     </div>
                     <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
--- src/Issues.js
+++ src/Issues.js
@@ -3,7 +3,25 @@ import React from 'react';
 function Issues() {
   return (
     <div className="Issues">
-        <h1>Issues</h1>
+        <section className='bg-light'>
+          <div className="container">
+            <div className="row">
+              <div className="col-lg-1"></div>
+              <div className="col-lg-10">
+              <div className="card bg-dark text-white">
+              <img src={require('./assets/img/backgrounds/ericAction.jpg')} className="img-fluid" alt="Responsive image"/>
+                <div className="card-img-overlay d-flex flex-column justify-content-end">
+                <blockquote className="blockquote text-right">
+                <p className="mb-0">Pushing for a brighter tomorrow.</p>
+                <footer className="blockquote-footer">Someone famous in <cite title="Source Title">Source Title</cite></footer>
+              </blockquote>
+                </div>
+              </div>
+              </div>
+              <div className="col-lg-1"></div>
+            </div>
+          </div>
+        </section>
     </div>
   );
 }
